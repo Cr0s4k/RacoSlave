97a45290322d443cd62d4cd37d59c206d71a1020
Omar Garcia <omarcruz11@hotmail.com>
2018-06-20 21:05:37 +0200

    Main refactoring

--- main.py
+++ main.py
@@ -1,57 +1,53 @@
 import telebot
-from configuration import Configuration
 import json
 from pprint import pprint
+import subprocess
+import time
 from scrapper import Scrapper
+from configuration import Configuration
 
 scrapper = Scrapper()
 config = Configuration.read_configuration()
 token = config["botToken"]
 bot = telebot.TeleBot(token)
-_continue = True
+running = False
+firstTime = True
 
 @bot.message_handler(commands=['Start'])
 def send_welcome(message):
-    bot.reply_to(message, "Empezemos!")
+    global running, last, firstTime
 
-    import subprocess
-    import time
-    global _continue, last
-    
-    _continue = True
-    while(_continue):
-        result = scrapper.getResult()
+    if not running:
+        bot.reply_to(message, "Let's get started!")
+        
+        running = True
+        while(running):
+            result = scrapper.getResult()
 
-        pprint("RESULT")
-        pprint(result)
+            if(result != last):
+                last = result
+                if not firsTime: bot.reply_to(message, "New news!")
+                scrapper.writeResult(result)
+            
+            if firstTime: firstTime = False
 
-        pprint("LAST")
-        pprint(last)
-
-        if(result != last):
-            last = result
-            bot.reply_to(message, "NUEVA NOTICIA")
-            scrapper.writeResult(result)
-        else:
-            bot.reply_to(message, "No news...")
-
-        time.sleep(10)
+            time.sleep(60 * 30)
 
 @bot.message_handler(commands=['Stop'])
 def send_finish(message):
-    global _continue
-    _continue = False;
-    bot.reply_to(message, "El programa ha parado.")
+    global running
+    running = False;
+    bot.reply_to(message, "Program has stopped")
 
 @bot.message_handler(commands=['Show'])
 def send_finish(message):
-    file = open("last.txt", "r")
-    bot.reply_to(message, file.read())
-    file.close()
+    global last
+    bot.reply_to(message, str(last))
 
 @bot.message_handler(func=lambda m:True)
 def echo_all(message):
     bot.reply_to(message, message.text)
+
 
 try:
     with open(config["resultFile"], "r") as file:
@@ -59,6 +55,5 @@ try:
         last = json.load(file)
 except:
     last = {}
-
 
 bot.polling()
